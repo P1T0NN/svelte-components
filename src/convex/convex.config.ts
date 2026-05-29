ccf2f6c42d355cc6d21fab020e33e88b54449a5b
P1T0NN <ognjen.tapuskovic@gmail.com>
2026-05-29 15:59:46 +0200

    Updated SvelteHead

--- src/convex/convex.config.ts
+++ src/convex/convex.config.ts
@@ -3,10 +3,12 @@ import { defineApp } from "convex/server";
 import rateLimiter from "@convex-dev/rate-limiter/convex.config.js";
 import r2 from "@convex-dev/r2/convex.config.js";
 import betterAuth from './auth/component/convex.config';
+import resend from "@convex-dev/resend/convex.config.js";
 
 const app = defineApp();
 app.use(rateLimiter);
 app.use(betterAuth);
 app.use(r2);
+app.use(resend);
 
 export default app;
